0847e1f0acb47c5a80363c77173103c7ed83c58b
Forest Dussault <forest.dussault@canada.ca>
2018-08-01 09:58:25 -0400

    Successful rub submitted page added

--- miseq_portal/miseq_uploader/urls.py
+++ miseq_portal/miseq_uploader/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 
 from miseq_uploader.views import (
     run_form_view,
-    # run_submitted_view
+    run_submitted_view
 )
 
 app_name = "miseq_uploader"
 urlpatterns = [
     path("", view=run_form_view, name="miseq_uploader"),
-    # path("run_submitted/", view=run_submitted_view, name="run_submitted"),
+    path("run_submitted/", view=run_submitted_view, name="run_submitted"),
 ]
--- miseq_portal/miseq_uploader/views.py
+++ miseq_portal/miseq_uploader/views.py
@@ -1,19 +1,36 @@
 from django.views.generic.edit import FormView
-from django.views.generic import TemplateView
-from .forms import RunForm
+from django.views.generic import View
+from django.shortcuts import render, redirect
+
+from .forms import RunModelForm
 
 
-class RunFormView(FormView):
-    form_class = RunForm
+class RunFormView(View):
+    form_class = RunModelForm
     template_name = 'miseq_uploader/miseq_uploader.html'
     success_url = 'miseq_uploader/run_submitted.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(self.success_url)
+        else:
+            return render(request, self.template_name, {'form': form})
 
 
 run_form_view = RunFormView.as_view()
 
 
-# class RunSubmittedView(FormView):
-#     template_name = 'miseq_uploader/run_submitted.html'
-#
-#
-# run_submitted_view = RunSubmittedView.as_view()
+class RunSubmittedView(View):
+    template_name = 'miseq_uploader/run_submitted.html'
+
+    def post(self, request):
+        return render(request, self.template_name, {})
+
+
+run_submitted_view = RunSubmittedView.as_view()
