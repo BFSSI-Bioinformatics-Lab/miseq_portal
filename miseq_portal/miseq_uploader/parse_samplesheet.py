4e8bb5244801253bd3910460b5843d2abc8f619b
Forest Dussault <forest.dussault@canada.ca>
2018-08-01 09:57:46 -0400

    Parsing updates

--- miseq_portal/miseq_uploader/parse_samplesheet.py
+++ miseq_portal/miseq_uploader/parse_samplesheet.py
@@ -9,8 +9,8 @@ from pathlib import Path
 def validate_samplesheet_header(header: list) -> bool:
     """
     Validates that column names match expected values
-    :param header:
-    :return:
+    :param header: List of column names
+    :return: True if header meets all expected values, False if not
     """
     expected_header = [
         'Sample_ID',
@@ -30,8 +30,8 @@ def validate_samplesheet_header(header: list) -> bool:
 def read_samplesheet(samplesheet: Path) -> pd.DataFrame:
     """
     Reads SampleSheet.csv and returns dataframe (all header information will be stripped)
-    :param samplesheet:
-    :return:
+    :param samplesheet: Path to SampleSheet.csv
+    :return: pandas df of SampleSheet.csv with head section stripped away
     """
     # TODO: Verify that it's always 22 rows that need to be skipped. Will need to change approach if it varies.
     df = pd.read_csv(samplesheet, sep=",", index_col=False, skiprows=22)
@@ -41,8 +41,8 @@ def read_samplesheet(samplesheet: Path) -> pd.DataFrame:
 def get_sample_id_list(samplesheet_df: pd.DataFrame) -> list:
     """
     Returns list of all SampleIDs in SampleSheet dataframe
-    :param samplesheet_df:
-    :return:
+    :param samplesheet_df: df returned from read_samplesheet()
+    :return: list of all Sample IDs
     """
     sample_id_list = list(samplesheet_df['Sample_ID'])
     return sample_id_list
@@ -50,15 +50,30 @@ def get_sample_id_list(samplesheet_df: pd.DataFrame) -> list:
 
 def group_by_project(samplesheet_df: pd.DataFrame) -> dict:
     """
-    Groups samples by project in extracted from SampleSheet.csv
-    :param samplesheet_df:
-    :return:
+    Groups samples by project extracted from SampleSheet.csv.
+    :param samplesheet_df: df returned from read_samplesheet()
+    :return: project dictionary (Keys are project names, values are lists of associated samples)
     """
     project_list = list(samplesheet_df.groupby(['Sample_Project']).groups.keys())
     project_dict = {}
     for project in project_list:
         project_dict[project] = list(samplesheet_df[samplesheet_df['Sample_Project'] == project]['Sample_ID'])
     return project_dict
+
+
+def extract_run_name(samplesheet: Path) -> str:
+    """
+    Retrieves the 'Experiment Name' from SampleSheet.csv
+    :param samplesheet: Path to SampleSheet.csv
+    :return: value of 'Experiment Name'
+    """
+    with open(str(samplesheet)) as f:
+        for line in f:
+            if 'Experiment Name' in line:
+                experiment_name = line.split(',')[1].strip()
+                return experiment_name
+        else:
+            raise Exception(f"Could not find 'Experiment Name' in {samplesheet}")
 
 
 def validate_sample_id(value: str, length: int = 15) -> bool:
