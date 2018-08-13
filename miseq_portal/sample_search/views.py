5b84ea41df77f5808cd33d5d6ddd90fcb2da032a
Forest Dussault <forest.dussault@canada.ca>
2018-08-13 14:51:14 -0400

    Added LoginRequiredMixin

--- miseq_portal/sample_search/views.py
+++ miseq_portal/sample_search/views.py
@@ -1,14 +1,13 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 
 from miseq_viewer.models import Sample
 
 
-class SampleSearchView(ListView):
+class SampleSearchView(LoginRequiredMixin, ListView):
     template_name = 'sample_search/sample_search.html'
     model = Sample
     context_object_name = 'sample_list'
-
-
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
