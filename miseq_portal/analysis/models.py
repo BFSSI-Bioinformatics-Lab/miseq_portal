fe19d67d4df779e21fffe6d401d4a4b59fb67292
Forest Dussault <forest.dussault@canada.ca>
2018-08-14 14:52:10 -0400

    Added SampleAnalysisGroup model. Not sure if it will be used...

--- miseq_portal/analysis/models.py
+++ miseq_portal/analysis/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from core.models import TimeStampedModel
+from miseq_viewer.models import Sample
+
 
 # Create your models here.
+class SampleAnalysisGroup(TimeStampedModel):
+    """
+    Temporary table to store Samples destined for analysis.
+    Should be cleared out whenever an analysis is submitted.
+    """
+    sample_id = models.OneToOneField(Sample, on_delete=models.CASCADE, primary_key=True)
+    analysis_type = models.CharField(max_length=256, blank=True)
+
+    def __str__(self):
+        return self.sample_id
