b13fd9fcbf16c1b48794b9ab9060195962621c43
Forest Dussault <forest.dussault@canada.ca>
2018-08-14 09:29:32 -0400

    Added SampleSheet viewing, changed view for Project Detail

--- miseq_portal/miseq_viewer/views.py
+++ miseq_portal/miseq_viewer/views.py
@@ -1,8 +1,12 @@
+from pathlib import Path
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils import timezone
 from django.views.generic import DetailView, ListView
 
+from config.settings.base import MEDIA_ROOT
+
 from miseq_viewer.models import Project, Run, Sample
+from miseq_uploader import parse_samplesheet
 
 
 class ProjectListView(LoginRequiredMixin, ListView):
@@ -28,6 +32,7 @@ class ProjectDetailView(LoginRequiredMixin, DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['run_list'] = Run.objects.all()
+        context['sample_list'] = Sample.objects.filter(project_id=context['project'])
         return context
 
 
@@ -43,6 +48,8 @@ class RunDetailView(LoginRequiredMixin, DetailView):
         context = super().get_context_data(**kwargs)
         context['project_list'] = Project.objects.all()
         context['sample_list'] = Sample.objects.all()
+        context['samplesheet_df'] = parse_samplesheet.read_samplesheet_to_html(
+            Path(MEDIA_ROOT) / str(context['run'].sample_sheet))
         return context
 
 
