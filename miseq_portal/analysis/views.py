f17dd57202af724a8076e28673e2d7055a90afe9
Forest Dussault <forest.dussault@canada.ca>
2018-08-14 14:51:33 -0400

    Initial commit

--- miseq_portal/analysis/views.py
+++ miseq_portal/analysis/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+
+from miseq_viewer.models import Sample
+
+
+class AnalysisIndexView(LoginRequiredMixin, ListView):
+    template_name = 'analysis/analysis_index.html'
+    model = Sample
+    context_object_name = 'sample_list'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        # Grab selected sample list from the analysis_index.html page via AJAX
+        sample_id_list = request.POST.getlist('sample_id_list[]')
+
+        # Get corresponding Sample objects
+        sample_object_list = list()
+        for sample_id in sample_id_list:
+            sample_object = Sample.objects.get(sample_id=sample_id)
+            sample_object_list.append(sample_object)
+
+        for sample_object in sample_object_list:
+            print(sample_object)
+
+        return render(request, self.template_name, {})
+
+
+analysis_index_view = AnalysisIndexView.as_view()
