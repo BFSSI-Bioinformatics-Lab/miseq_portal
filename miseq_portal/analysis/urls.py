e941f13abb747ebd5852fc3bdcca3327a080be7c
Forest Dussault <forest.dussault@canada.ca>
2018-08-15 11:13:43 -0400

    Added the tool selection page

--- miseq_portal/analysis/urls.py
+++ miseq_portal/analysis/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from analysis.views import (
     analysis_index_view,
+    tool_selection_view,
 )
 
 app_name = "analysis"
 urlpatterns = [
     # Index
     path("", view=analysis_index_view, name="analysis_index"),
+    path("tool_selection/", view=tool_selection_view, name="tool_selection"),
 
 ]
