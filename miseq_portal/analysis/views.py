3a0cbc6aae45a2d729babba2bed29c48e8bdcff1
Forest Dussault <forest.dussault@canada.ca>
2018-08-15 11:14:41 -0400

    Added POST logic via AJAX for the Sample Selection view

--- miseq_portal/analysis/views.py
+++ miseq_portal/analysis/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import ListView, View
 
 from miseq_viewer.models import Sample
+from analysis.models import SampleAnalysisTemporaryGroup
 
 
 class AnalysisIndexView(LoginRequiredMixin, ListView):
     template_name = 'analysis/analysis_index.html'
+    success_url = 'tool_selection/'
     model = Sample
     context_object_name = 'sample_list'
 
@@ -16,19 +19,41 @@ class AnalysisIndexView(LoginRequiredMixin, ListView):
         return context
 
     def post(self, request, *args, **kwargs):
-        # Grab selected sample list from the analysis_index.html page via AJAX
+        # Grab sample list from user selection from the analysis_index.html page via AJAX
         sample_id_list = request.POST.getlist('sample_id_list[]')
 
-        # Get corresponding Sample objects
+        # Get corresponding Sample objects from user selection
         sample_object_list = list()
         for sample_id in sample_id_list:
             sample_object = Sample.objects.get(sample_id=sample_id)
             sample_object_list.append(sample_object)
 
-        for sample_object in sample_object_list:
-            print(sample_object)
+        # Delete all rows in SampleObjectTemp table belonging to requesting user
+        SampleAnalysisTemporaryGroup.objects.filter(user=request.user).delete()
 
-        return render(request, self.template_name, {})
+        # Add objects to SampleAnalysisTemporaryGroup table
+        for sample_object in sample_object_list:
+            obj = SampleAnalysisTemporaryGroup(sample_id=sample_object, user=request.user)
+            obj.save()
+
+        context = {'success': True,
+                   'url': 'tool_selection/'}
+        return JsonResponse(context)
 
 
 analysis_index_view = AnalysisIndexView.as_view()
+
+
+class ToolSelectionView(LoginRequiredMixin, View):
+    template_name = 'analysis/tool_selection.html'
+
+    def get(self, request):
+        sample_group = SampleAnalysisTemporaryGroup.objects.all()
+
+        context = {
+            'sample_group': sample_group
+        }
+        return render(request, self.template_name, context)
+
+
+tool_selection_view = ToolSelectionView.as_view()
