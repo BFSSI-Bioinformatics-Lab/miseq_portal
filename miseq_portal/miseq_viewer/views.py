83ba3309d778b3dd7a074a736af0445561e6b815
bfssi-forest-dussault <forest.dussault@canada.ca>
2018-08-28 11:20:15 -0400

    Added context for overview chart for the ProjectList view

--- miseq_portal/miseq_viewer/views.py
+++ miseq_portal/miseq_viewer/views.py
@@ -1,7 +1,9 @@
+import json
 from pathlib import Path
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils import timezone
-from django.views.generic import DetailView, ListView
+from django.shortcuts import HttpResponse, Http404
+from django.views.generic import DetailView, ListView, View
 
 from config.settings.base import MEDIA_ROOT
 
@@ -19,7 +21,16 @@ class ProjectListView(LoginRequiredMixin, ListView):
         context['now'] = timezone.now()
         context['user'] = self.request.user
         context['approved_users'] = UserProjectRelationship.objects.filter(user_id=self.request.user)
+        context['overview_json'] = self.get_overview_data()
         return context
+
+    @staticmethod
+    def get_overview_data():
+        overview_dict = dict()
+        overview_dict['number_of_projects'] = len(Project.objects.all().values())
+        overview_dict['number_of_samples'] = len(Sample.objects.all().values())
+        overview_dict['number_of_runs'] = len(Run.objects.all().values())
+        return json.dumps(overview_dict)
 
 
 project_list_view = ProjectListView.as_view()
@@ -68,3 +79,21 @@ class SampleDetailView(LoginRequiredMixin, DetailView):
 
 
 sample_detail_view = SampleDetailView.as_view()
+
+# class DownloadReadView(LoginRequiredMixin, DetailView):
+#     model = Sample
+#     context_object_name = 'sample'
+#     # template_name = "miseq_viewer/sample_download.html"
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#
+#     def download(self, request, path):
+#         file_path = os.path.join(MEDIA_ROOT, path)
+#         if os.path.exists(file_path):
+#             with open(file_path, 'rb') as fh:
+#                 response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+#                 response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+#                 return response
+#         raise Http404
+#
