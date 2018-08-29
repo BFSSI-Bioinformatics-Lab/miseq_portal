40be14a4ebf5287f5c3246b30fd4f7f597439af6
bfssi-forest-dussault <forest.dussault@canada.ca>
2018-08-29 09:03:11 -0400

    Registered RunInterOpData

--- miseq_portal/miseq_viewer/admin.py
+++ miseq_portal/miseq_viewer/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from .models import Project, Run, Sample, UserProjectRelationship, SampleLogData
+from .models import Project, Run, RunInterOpData, Sample, UserProjectRelationship, SampleLogData
 
 
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(Run)
+admin.site.register(RunInterOpData)
 admin.site.register(Sample)
 admin.site.register(UserProjectRelationship)
 admin.site.register(SampleLogData)
