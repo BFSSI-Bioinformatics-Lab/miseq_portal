8b65443670892cd480ea9127f51589a4c936126c
bfssi-forest-dussault <forest.dussault@canada.ca>
2018-08-30 10:20:21 -0400

    Added json method

--- miseq_portal/miseq_uploader/parse_interop.py
+++ miseq_portal/miseq_uploader/parse_interop.py
@@ -14,7 +14,7 @@ def get_qscore_dataframe(run_folder: Path) -> pd.DataFrame:
     run_metrics = py_interop_run_metrics.run_metrics()
     valid_to_load = py_interop_run.uchar_vector(py_interop_run.MetricCount, 0)
     valid_to_load[py_interop_run.Q] = 1
-    run_metrics.read(run_folder, valid_to_load)
+    run_metrics.read(str(run_folder), valid_to_load)
     bar_data = py_interop_plot.bar_plot_data()
     boundary = 30
     options = py_interop_plot.filter_options(run_metrics.run_info().flowcell().naming_method())
@@ -29,3 +29,8 @@ def get_qscore_dataframe(run_folder: Path) -> pd.DataFrame:
     df_dict['y'] = y_vals
     df = pd.DataFrame.from_dict(df_dict)
     return df
+
+
+def get_qscore_json(run_folder: Path) -> str:
+    df = get_qscore_dataframe(run_folder)
+    return df.to_json()
