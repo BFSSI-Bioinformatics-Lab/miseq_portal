b5cf01d9004efede6327c574fb309b6a43986987
bfssi-forest-dussault <forest.dussault@canada.ca>
2018-09-18 15:38:39 -0400

    Initial commit

---
+++ miseq_portal/analysis/tools/plasmid_report.py
@@ -0,0 +1,4 @@
+"""
+1. Attempt to reconstruct plasmid(s) with MOB-suite mob_recon https://github.com/phac-nml/mob-suite
+2. Conduct AMR profiling on extracted plasmid(s) with ABRicate https://github.com/tseemann/abricate
+"""
