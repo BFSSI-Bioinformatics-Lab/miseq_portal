1215cfe40a734d2cf27b999c04e315bc68afd252
bfssi-forest-dussault <forest.dussault@canada.ca>
2018-09-19 15:48:25 -0400

    Registered new models

--- miseq_portal/analysis/admin.py
+++ miseq_portal/analysis/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from .models import AnalysisSample, AnalysisGroup
+from .models import AnalysisSample, AnalysisGroup, \
+    SendsketchResult, MobSuiteAnalysisGroup, \
+    MobSuiteAnalysisPlasmid
 
 # Register your models here.
 admin.site.register(AnalysisSample)
 admin.site.register(AnalysisGroup)
+admin.site.register(SendsketchResult)
+admin.site.register(MobSuiteAnalysisGroup)
+admin.site.register(MobSuiteAnalysisPlasmid)
