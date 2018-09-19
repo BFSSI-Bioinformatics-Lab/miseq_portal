b657f7b9a03abf2fd05f73af809818713c7b0f46
bfssi-forest-dussault <forest.dussault@canada.ca>
2018-09-19 11:13:29 -0400

    Sample and SampleAssemblyData are now searchable in admin panel

--- miseq_portal/miseq_viewer/admin.py
+++ miseq_portal/miseq_viewer/admin.py
@@ -3,11 +3,19 @@ from django.contrib import admin
 from .models import Project, Run, RunInterOpData, Sample, UserProjectRelationship, SampleLogData, SampleAssemblyData
 
 
+class SampleAdmin(admin.ModelAdmin):
+    search_fields = ['sample_id']
+
+
+class SampleAssemblyDataAdmin(admin.ModelAdmin):
+    search_fields = ['sample_id__sample_id']
+
+
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(Run)
 admin.site.register(RunInterOpData)
-admin.site.register(Sample)
+admin.site.register(Sample, SampleAdmin)
 admin.site.register(UserProjectRelationship)
 admin.site.register(SampleLogData)
-admin.site.register(SampleAssemblyData)
+admin.site.register(SampleAssemblyData, SampleAssemblyDataAdmin)
