df284a291307654df7b2c320ab2a6dcf0b026336
bfssi-forest-dussault <forest.dussault@canada.ca>
2018-09-20 15:15:00 -0400

    Implemented mob_recon

--- miseq_portal/analysis/tools/plasmid_report.py
+++ miseq_portal/analysis/tools/plasmid_report.py
@@ -11,7 +11,7 @@ General overview:
 
 from pathlib import Path
 from dataclasses import dataclass
-from analysis.tools.helpers import run_subprocess
+from miseq_portal.analysis.tools.helpers import run_subprocess
 from config.settings.base import MOB_SUITE_PATH, ABRICATE_PATH, STAR_AMR_PATH
 
 
@@ -23,18 +23,19 @@ class MobSuiteDataObject:
     plasmid_fasta_list: [Path]
 
 
-def call_mob_suite(assembly: Path, outdir: Path) -> MobSuiteDataObject:
+def call_mob_recon(assembly: Path, outdir: Path) -> MobSuiteDataObject:
     mob_recon = MOB_SUITE_PATH / 'mob_recon'
-    cmd = f'{mob_recon} --infile {assembly} --outdir {outdir}'
+    # cmd = f'{mob_recon} --infile {assembly} --outdir {outdir} --run_typer'
+    cmd = f'mob_recon --infile {assembly} --outdir {outdir} --run_typer'
     run_subprocess(cmd)
     contig_report = list(outdir.glob("contig_report.txt"))[0]
     mobtyper_aggregate_report = list(outdir.glob("mobtyper_aggregate_report.txt"))[0]
     plasmid_fasta_list = list(outdir.glob("plasmid_*.fasta"))
-    mob_suite_data = MobSuiteDataObject(original_contigs=assembly,
+    mob_recon_data = MobSuiteDataObject(original_contigs=assembly,
                                         contig_report=contig_report,
                                         mobtyper_aggregate_report=mobtyper_aggregate_report,
                                         plasmid_fasta_list=plasmid_fasta_list)
-    return mob_suite_data
+    return mob_recon_data
 
 
 @dataclass
