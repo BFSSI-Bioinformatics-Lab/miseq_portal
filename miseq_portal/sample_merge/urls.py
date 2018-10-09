4afcce4373cd3df9908b6a69d555d7bc4a6e9360
bfssi-forest-dussault <forest.dussault@canada.ca>
2018-10-09 15:43:08 -0400

    Added success path

--- miseq_portal/sample_merge/urls.py
+++ miseq_portal/sample_merge/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, re_path
 
-from miseq_portal.sample_merge.views import (sample_merge_index_view)
+from miseq_portal.sample_merge.views import (sample_merge_index_view, merge_success_view)
 
 app_name = "sample_merge"
 urlpatterns = [
     # Index
     path("", view=sample_merge_index_view, name="sample_merge_index"),
-    # re_path("^my_jobs/(?P<pk>\d+)$", view=analysis_group_detail_view, name="analysis_group_detail_view")
+    re_path("^merge_success/", view=merge_success_view, name="merge_success_view")
 ]
