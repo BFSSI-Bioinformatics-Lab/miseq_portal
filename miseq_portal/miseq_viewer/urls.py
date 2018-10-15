0dfc447fa6e9b2d5a0630d0f3d8874e8e9622551
bfssi-forest-dussault <forest.dussault@canada.ca>
2018-10-15 15:43:04 -0400

    URL consistency

--- miseq_portal/miseq_viewer/urls.py
+++ miseq_portal/miseq_viewer/urls.py
@@ -10,7 +10,7 @@ from miseq_portal.miseq_viewer.views import (
 app_name = "miseq_viewer"
 urlpatterns = [
     path("", view=project_list_view, name="miseq_viewer_projects"),
-    re_path("^project/(?P<pk>\d+)/$", view=project_detail_view, name="miseq_viewer_project_detail"),
+    re_path("^project/(?P<pk>\d+)$", view=project_detail_view, name="miseq_viewer_project_detail"),
     re_path("^run/(?P<pk>\d+)$", view=run_detail_view, name="miseq_viewer_run_detail"),
     re_path("^sample/(?P<pk>\d+)$", view=sample_detail_view,
             name="miseq_viewer_sample_detail")
