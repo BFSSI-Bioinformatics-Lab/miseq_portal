b1332b22c2e729c471552517b40e38cb5766dc03
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-01-10 09:53:50 -0500

    Docstrings

--- miseq_portal/analysis/tools/helpers.py
+++ miseq_portal/analysis/tools/helpers.py
@@ -1,9 +1,19 @@
+"""
+Generic functions for miseq_portal tools to call
+"""
+
 import os
 from pathlib import Path
 from subprocess import Popen, PIPE
 
 
-def run_subprocess(cmd: str, get_stdout=False):
+def run_subprocess(cmd: str, get_stdout=False) -> str:
+    """
+    Wrapper for subprocess.Popen()
+    :param cmd: String containing command to pass to system
+    :param get_stdout: Flag to grab stdout/stderr and return as str
+    :return: If get_stdout, returns str of stdout/stderr, else doesn't return anything
+    """
     if get_stdout:
         p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
         out, err = p.communicate()
@@ -20,7 +30,11 @@ def run_subprocess(cmd: str, get_stdout=False):
         p.wait()
 
 
-def remove_dir_files(outdir: Path):
-    to_delete = list(outdir.glob("*.*"))
+def remove_dir_files(target_directory: Path):
+    """
+    Delete all of the files in a given directory
+    :param target_directory: Path to directory to delete files in
+    """
+    to_delete = list(target_directory.glob("*.*"))
     for f in to_delete:
         os.remove(str(f))
