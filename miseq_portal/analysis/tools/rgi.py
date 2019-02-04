f9a2c2c9998ac4743770e12a6bffe283eaf65592
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-02-04 11:15:05 -0500

    Initial commit

---
+++ miseq_portal/analysis/tools/rgi.py
@@ -0,0 +1,8 @@
+"""
+TODO: Implement RGI for AMR detection.
+
+Example cmd:
+rgi main -i {input_file} -i {outdir} --clean
+
+RGI also has subcommands to generate visuals (parser, heatmap), create summaries for > 1 samples (tab)
+"""
