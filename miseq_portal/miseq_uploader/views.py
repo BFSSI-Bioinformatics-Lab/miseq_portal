56e1b86591cf23adfdf93e66cfba3b23158e2870
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-02-11 15:07:54 -0500

    Fixed imports; accommodated removal of deprecated URLs, removed old code

--- miseq_portal/miseq_uploader/views.py
+++ miseq_portal/miseq_uploader/views.py
@@ -1,19 +1,18 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.contrib.admin.views.decorators import staff_member_required
-from django.utils.decorators import method_decorator
-from django.views.generic import View, TemplateView
-from django.shortcuts import render, redirect
-from django.http import Http404
-
+import logging
 from pathlib import Path
 
-from .forms import RunModelForm, CreateProjectForm, UploadMiSeqDirectoryForm
-from miseq_portal.miseq_viewer.models import Project
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.http import Http404
+from django.shortcuts import render, redirect
+from django.utils.decorators import method_decorator
+from django.views.generic import View, TemplateView
+
+from miseq_portal.miseq_uploader.forms import UploadMiSeqDirectoryForm
 from miseq_portal.miseq_uploader.upload_to_db import receive_miseq_run_dir
 
-import logging
-
-logger = logging.getLogger('raven')
+# logger = logging.getLogger('raven')
+logger = logging.getLogger(__name__)
 
 
 @method_decorator(staff_member_required, name='dispatch')
@@ -33,32 +32,6 @@ class SampleFormView(LoginRequiredMixin, View):
 
 
 sample_form_view = SampleFormView.as_view()
-
-
-@method_decorator(staff_member_required, name='dispatch')
-class CreateProjectView(LoginRequiredMixin, View):
-    form_class = CreateProjectForm
-    template_name = 'miseq_uploader/create_project.html'
-    success_url = 'project_created/'
-
-    def get(self, request):
-        form = self.form_class()
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request):
-        form = self.form_class(request.POST, request.FILES)
-
-        if form.is_valid():
-            obj = form.save(commit=False)
-            obj.project_id = request.POST['project_id']
-            obj.save()
-            return redirect(self.success_url)
-        else:
-            logger.info('Form not valid')
-            return render(request, self.template_name, {'form': form})
-
-
-create_project_view = CreateProjectView.as_view()
 
 
 @method_decorator(staff_member_required, name='dispatch')
@@ -87,69 +60,8 @@ class MiSeqFormView(LoginRequiredMixin, UserPassesTestMixin, View):
             receive_miseq_run_dir(Path(request.POST['miseq_directory']))
             return redirect(self.success_url)
         else:
-            logger.info('something bad happened??')
+            logging.warning("Could not submit form!")
             return render(request, self.template_name, {'form': form})
 
 
 miseq_form_view = MiSeqFormView.as_view()
-
-
-@method_decorator(staff_member_required, name='dispatch')
-class RunFormView(LoginRequiredMixin, View):
-    form_class = RunModelForm
-    template_name = 'miseq_uploader/run_uploader.html'
-    success_url = 'run_submitted/'
-
-    def get(self, request):
-        form = self.form_class()
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request):
-        # Get copy of POST
-        post = request.POST.copy()
-
-        # Update project_id field
-        project_id = post.get('project_id')
-        try:
-            project = Project.objects.get(project_id=project_id)
-        except Project.DoesNotExist:
-            logger.info(f'Could not retrieve the following project: {project_id}')
-            return
-        post['project_id'] = project
-        post['project_id_id'] = project.pk
-
-        # Reset POST
-        request.POST = post
-
-        form = self.form_class(request.POST, request.FILES)
-
-        if form.is_valid():
-            obj = form.save(commit=False)
-            obj.run_id = post['run_id']
-            obj.project_id_id = post['project_id_id']  # Have to manually save the pk of the project in this column
-            obj.sample_sheet = request.FILES['sample_sheet']
-            obj.save()
-            return redirect(self.success_url)
-        else:
-            # TODO: Make this better
-            logger.info('Form not valid')
-            return render(request, self.template_name, {'form': form})
-
-
-run_form_view = RunFormView.as_view()
-
-
-@method_decorator(staff_member_required, name='dispatch')
-class RunSubmittedView(LoginRequiredMixin, TemplateView):
-    template_name = 'miseq_uploader/run_submitted.html'
-
-
-run_submitted_view = RunSubmittedView.as_view()
-
-
-@method_decorator(staff_member_required, name='dispatch')
-class ProjectCreatedView(LoginRequiredMixin, TemplateView):
-    template_name = 'miseq_uploader/project_created.html'
-
-
-project_created_view = ProjectCreatedView.as_view()
