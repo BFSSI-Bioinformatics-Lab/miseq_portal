f5471b7a4e2f935d1af7add73b9131884151cd82
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-02-11 15:04:44 -0500

    Removed old code; changed logging

--- miseq_portal/miseq_uploader/forms.py
+++ miseq_portal/miseq_uploader/forms.py
@@ -1,10 +1,7 @@
-from django import forms
-from django.forms import ModelForm, Form
-
-from miseq_portal.users.models import User
-from miseq_portal.miseq_viewer.models import Run, Project
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
+from django import forms
+from django.forms import Form
 
 
 # MISEQ DIRECTORY
@@ -20,86 +17,3 @@ class UploadMiSeqDirectoryForm(Form):
         self.helper.form_action = 'upload_miseq_directory'
         self.fields['miseq_directory'].label = 'MiSeq Directory'
         self.helper.add_input(Submit('submit', 'Submit'))
-
-
-# PROJECT
-class CreateProjectForm(ModelForm):
-    project_id = forms.CharField(label="Project ID", required=True)
-    project_owner = forms.ModelChoiceField(queryset=User.objects.all())
-
-    def __init__(self, *args, **kwargs):
-        super(CreateProjectForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_id = 'id-uploadRunForm'
-        self.helper.form_class = 'blueForms'
-        self.helper.form_method = 'post'
-        self.helper.form_action = 'create_project'
-        self.fields['project_id'].label = 'Project ID'
-        self.fields['project_owner'].label = 'Project Owner'
-        self.helper.add_input(Submit('submit', 'Submit'))
-
-    class Meta:
-        model = Project
-        fields = ['project_id', 'project_owner']
-
-
-# RUN
-class RunModelForm(ModelForm):
-    # Retrieve unique Project IDs and display in dropdown
-    try:
-        iquery = Project.objects.values_list('project_id', flat=True)
-        iquery_choices = sorted([(id, id) for id in iquery])
-        project_id = forms.ChoiceField(choices=iquery_choices, required=True, widget=forms.Select(), label="Project ID")
-    except:
-        pass
-
-    # Run ID
-    run_id = forms.CharField(label="Run ID", required=True)
-
-    # SampleSheet.csv
-    sample_sheet = forms.FileField(widget=forms.FileInput(),
-                                   label='Sample Sheet',
-                                   required=True)
-
-    def clean_project_id(self):
-        data = self.cleaned_data['project_id']
-        project = Project.objects.get(project_id=data)
-        return project
-
-    def clean_run_id(self):
-        data = self.cleaned_data['run_id']
-        return data
-
-    def clean_sample_sheet(self):
-        data = self.cleaned_data['sample_sheet']
-        if 'csv' not in str(data):
-            raise forms.ValidationError("Provided SampleSheet does not end in .csv")
-
-    def __init__(self, *args, **kwargs):
-        super(RunModelForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_id = 'id-uploadRunForm'
-        self.helper.form_class = 'blueForms'
-        self.helper.form_method = 'post'
-        self.helper.form_action = 'run_submitted'
-
-        self.helper.add_input(Submit('submit', 'Submit'))
-
-    class Meta:
-        model = Run
-        fields = ['run_id', 'project_id', 'sample_sheet']
-
-# # SAMPLE
-# class SampleForm(forms.Form):
-#     fwd_reads = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': False}),
-#                                 label="*_R1*.fastq.gz",
-#                                 required=True)
-#     rev_reads = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': False}),
-#                                 label="*_R2*.fastq.gz",
-#                                 required=True)
-#
-#
-# class SampleModelForm(ModelForm):
-#     class Meta:
-#         model = Run
-#         fields = ['fwd_reads', 'rev_reads']
