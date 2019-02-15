d8bed433791be4efc80a0273ca3ec5c6637c245b
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-02-15 09:39:35 -0500

    Added route for the samples API

--- miseq_portal/miseq_viewer/urls.py
+++ miseq_portal/miseq_viewer/urls.py
@@ -4,12 +4,14 @@ from miseq_portal.miseq_viewer.views import (
     project_list_view,
     project_detail_view,
     run_detail_view,
-    sample_detail_view
+    sample_detail_view,
+    samples_api_view
 )
 
 app_name = "miseq_viewer"
 urlpatterns = [
     path("", view=project_list_view, name="miseq_viewer_projects"),
+    path("samples_api/", view=samples_api_view, name="miseq_viewer_samples_api"),
     re_path("^project/(?P<pk>\d+)$", view=project_detail_view, name="miseq_viewer_project_detail"),
     re_path("^run/(?P<pk>\d+)$", view=run_detail_view, name="miseq_viewer_run_detail"),
     re_path("^sample/(?P<pk>\d+)$", view=sample_detail_view,
