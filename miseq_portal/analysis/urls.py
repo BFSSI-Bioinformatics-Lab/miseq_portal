d32d68d7e19553fe19114e481eeb2d505b455354
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-02-25 13:44:37 -0500

    Helpful comments

--- miseq_portal/analysis/urls.py
+++ miseq_portal/analysis/urls.py
@@ -7,18 +7,30 @@ from miseq_portal.analysis.views import (
     job_submitted_view,
     analysis_group_detail_view,
     analysis_group_delete_view,
-    analysis_group_delete_success_view
+    analysis_group_delete_success_view,
+    sample_select_view
 )
 
 app_name = "analysis"
 urlpatterns = [
     # Index
     path("", view=analysis_index_view, name="analysis_index"),
+
+    # Page for selecting analysis tools
     path("tool_selection/", view=tool_selection_view, name="tool_selection"),
+
+    # Sample selector
+    path("sample_select/", view=sample_select_view, name="sample_select"),
+
+    # Successful job submission page
     path("tool_selection/job_submitted/", view=job_submitted_view, name="job_submitted"),
+
+    # Analysis jobs pages
     path("my_jobs/", view=my_jobs_view, name="my_jobs"),
     re_path("^my_jobs/(?P<pk>\d+)$", view=analysis_group_detail_view, name="analysis_group_detail_view"),
     re_path("^my_jobs/(?P<pk>\d+)/delete$", view=analysis_group_delete_view, name="analysis_group_delete_view"),
+
+    # Analysis deleted success page
     path("analysis_group_deleted/success", view=analysis_group_delete_success_view,
          name="analysis_group_delete_success_view")
 ]
