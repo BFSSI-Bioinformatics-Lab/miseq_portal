bc4270fbea1028842d682ad5963bc13790753da2
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-02-27 08:41:54 -0500

    Added serializers for User, Run, Project models

--- miseq_portal/miseq_viewer/serializers.py
+++ miseq_portal/miseq_viewer/serializers.py
@@ -1,11 +1,34 @@
 from rest_framework import serializers
 
-from miseq_portal.miseq_viewer.models import Sample
+from miseq_portal.miseq_viewer.models import Sample, Project, Run
+from miseq_portal.users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class RunSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Run
+        fields = '__all__'
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    project_owner = UserSerializer()
+
+    class Meta:
+        model = Project
+        fields = '__all__'
 
 
 class SampleSerializer(serializers.ModelSerializer):
+    run_id = RunSerializer()
+    project_id = ProjectSerializer()
+
     class Meta:
         model = Sample
         # fields = '__all__'
         fields = ['id', 'sample_id', 'sample_name', 'project_id', 'run_id', 'sample_type', 'fwd_reads', 'rev_reads']
-
