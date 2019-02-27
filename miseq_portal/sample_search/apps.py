ac92389e59114e5adec8b8033b8f3521ddae178e
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-02-27 14:08:45 -0500

    Typo

--- miseq_portal/sample_search/apps.py
+++ miseq_portal/sample_search/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class SampleSearchConfig(AppConfig):
     name = 'miseq_portal.sample_search'
-    verbose_name = "Sample Search"
+    verbose_name = 'Sample Search'
 
