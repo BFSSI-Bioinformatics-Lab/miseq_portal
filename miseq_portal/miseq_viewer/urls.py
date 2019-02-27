9a5eb330c9c17fe9ec8d784b08cff115d008b5ff
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-02-27 08:42:24 -0500

    Refactored to use routers for API - includes samples, runs, projects

--- miseq_portal/miseq_viewer/urls.py
+++ miseq_portal/miseq_viewer/urls.py
@@ -1,17 +1,27 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, include
+from rest_framework import routers
 
 from miseq_portal.miseq_viewer.views import (
     project_list_view,
     project_detail_view,
     run_detail_view,
     sample_detail_view,
-    samples_api_view
+    SampleViewSet, RunViewSet, ProjectViewSet
+    # samples_api_view
 )
 
 app_name = "miseq_viewer"
+
+# Django-rest-framework (https://django-rest-framework-datatables.readthedocs.io/en/latest/tutorial.html)
+router = routers.DefaultRouter()
+router.register(r'samples', SampleViewSet)
+router.register(r'runs', RunViewSet)
+router.register(r'projects', ProjectViewSet)
+
 urlpatterns = [
     path("", view=project_list_view, name="miseq_viewer_projects"),
-    path("samples_api/", view=samples_api_view, name="miseq_viewer_samples_api"),
+    # path("samples_api/", view=samples_api_view, name="miseq_viewer_samples_api"),
+    re_path("^api/", include(router.urls), name="miseq_viewer_api"),
     re_path("^project/(?P<pk>\d+)$", view=project_detail_view, name="miseq_viewer_project_detail"),
     re_path("^run/(?P<pk>\d+)$", view=run_detail_view, name="miseq_viewer_run_detail"),
     re_path("^sample/(?P<pk>\d+)$", view=sample_detail_view,
