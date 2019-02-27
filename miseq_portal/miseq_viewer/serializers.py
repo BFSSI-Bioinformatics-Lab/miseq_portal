697aaac0e95321bf25be1c87eebedac16d4529c5
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-02-27 11:29:49 -0500

    Added SendsketchResult serializer

--- miseq_portal/miseq_viewer/serializers.py
+++ miseq_portal/miseq_viewer/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from miseq_portal.analysis.models import SendsketchResult
 from miseq_portal.miseq_viewer.models import Sample, Project, Run
 from miseq_portal.users.models import User
 
@@ -24,11 +25,19 @@ class ProjectSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class SendsketchResultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SendsketchResult
+        fields = '__all__'
+
+
 class SampleSerializer(serializers.ModelSerializer):
     run_id = RunSerializer()
     project_id = ProjectSerializer()
+    sendsketchresult = SendsketchResultSerializer()
 
     class Meta:
         model = Sample
-        # fields = '__all__'
-        fields = ['id', 'sample_id', 'sample_name', 'project_id', 'run_id', 'sample_type', 'fwd_reads', 'rev_reads']
+        fields = ['id', 'sample_id', 'sample_name', 'project_id', 'run_id',
+                  'sample_type', 'fwd_reads', 'rev_reads', 'sendsketchresult'
+                  ]
