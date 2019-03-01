752cee0c3006ebf7f082b003fb3ae3114637dfe2
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-03-01 11:03:43 -0500

    Registered models

--- miseq_portal/sample_workbooks/admin.py
+++ miseq_portal/sample_workbooks/admin.py
@@ -1,1 +1,17 @@
 # Register your models here.
+from django.contrib import admin
+
+from miseq_portal.sample_workbooks.models import Workbook, WorkbookSample
+
+
+class WorkbookAdmin(admin.ModelAdmin):
+    search_fields = ['workbook_title']
+
+
+class WorkbookSampleAdmin(admin.ModelAdmin):
+    search_fields = ['sample']
+
+
+# Register your models here.
+admin.site.register(Workbook)
+admin.site.register(WorkbookSample)
