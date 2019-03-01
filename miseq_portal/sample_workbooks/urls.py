6478344a6af7f91beced3e7b197314a984f7b4e9
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-03-01 11:05:17 -0500

    Registered new URLs for Workbook app

--- miseq_portal/sample_workbooks/urls.py
+++ miseq_portal/sample_workbooks/urls.py
@@ -1,11 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from miseq_portal.sample_workbooks.views import (
-    sample_workbook_index_view
+    sample_workbook_index_view,
+    create_new_workbook_view,
+    workbook_detail_view
 )
 
 app_name = "sample_workbooks"
 urlpatterns = [
     path("", view=sample_workbook_index_view, name="sample_workbook_index"),
-
+    path("create/", view=create_new_workbook_view, name="create_new_workbook"),
+    re_path("^detail/(?P<pk>\d+)$", view=workbook_detail_view, name="workbook_detail"),
 ]
