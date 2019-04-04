e26dea4caca337d3867c33a20e78b1cb4b2ef81c
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-04-04 15:35:53 -0400

    Registered MashResult model

--- miseq_portal/analysis/admin.py
+++ miseq_portal/analysis/admin.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 
 from .models import AnalysisSample, AnalysisGroup, \
     SendsketchResult, MobSuiteAnalysisGroup, \
-    MobSuiteAnalysisPlasmid, RGIResult, RGIGroupResult
+    MobSuiteAnalysisPlasmid, RGIResult, RGIGroupResult, MashResult
 
 # Register your models here.
 admin.site.register(AnalysisSample)
 admin.site.register(AnalysisGroup)
 admin.site.register(SendsketchResult)
+admin.site.register(MashResult)
 admin.site.register(MobSuiteAnalysisGroup)
 admin.site.register(MobSuiteAnalysisPlasmid)
 admin.site.register(RGIResult)
