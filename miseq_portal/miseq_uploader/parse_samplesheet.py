bbd9f67490898d33a18d0a6281847ccb912eec59
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-04-04 15:38:19 -0400

    Added iSeq support; samplesheet parsing; some variable refactoring

--- miseq_portal/miseq_uploader/parse_samplesheet.py
+++ miseq_portal/miseq_uploader/parse_samplesheet.py
@@ -10,42 +10,47 @@ logger = logging.getLogger('django')
 
 def validate_samplesheet_header(header: list) -> bool:
     """
-    Validates that column names match expected values
+    Validates that column names match expected values.
+    Expected column names for both iSeq and MiSeq sequencers are hardcoded here.
     :param header: List of column names
-    :return: True if header meets all expected values, False if not
+    :return: True if header meets all expected values, else False
     """
-    expected_header = [
-        'Sample_ID',
-        'Sample_Name',
-        'Sample_Plate',
-        'Sample_Well',
-        'I7_Index_ID',
-        'index',
-        'I5_Index_ID',
-        'index2',
-        'Sample_Project',
-        'Description'
-    ]
-    if not set(header) == set(expected_header):
-        raise Exception(f"Provided header {header} does not match expected header {expected_header}")
-    else:
-        return True
+    header = sorted(header)
+    expected_miseq_header = sorted(['Sample_ID', 'Sample_Name',
+                                    'Sample_Plate', 'Sample_Well',
+                                    'I7_Index_ID', 'index',
+                                    'I5_Index_ID', 'index2',
+                                    'Sample_Project', 'Description'
+                                    ])
+
+    expected_iseq_header = sorted(['Sample_ID', 'Sample_Name', 'Description',
+                                   'I7_Index_ID', 'index',
+                                   'I5_Index_ID', 'index2',
+                                   'Sample_Project'
+                                   ])
+
+    if not set(header) == set(expected_miseq_header) and not set(header) == set(expected_iseq_header):
+        raise Exception(
+            f"Provided header {header} does not match expected header.\nExpected:\n"
+            f"MiSeq:\t{expected_miseq_header} or \n"
+            f"iSeq:\t{expected_iseq_header}")
+    return True
 
 
-def read_samplesheet(sample_sheet: Path) -> pd.DataFrame:
+def read_samplesheet(samplesheet: Path) -> pd.DataFrame:
     """
     Reads SampleSheet.csv and returns dataframe (all header information will be stripped)
-    :param sample_sheet: Path to SampleSheet.csv
+    :param samplesheet: Path to SampleSheet.csv
     :return: pandas df of SampleSheet.csv with head section stripped away
     """
     counter = 1
-    with open(str(sample_sheet)) as f:
+    with open(str(samplesheet)) as f:
         for line in f:
             if '[Data]' in line:
                 break
             else:
                 counter += 1
-    df = pd.read_csv(sample_sheet, sep=",", index_col=False, skiprows=counter)
+    df = pd.read_csv(samplesheet, sep=",", index_col=False, skiprows=counter)
 
     # Force Sample_Name and Sample_Project into str types
     df['Sample_Name'] = df['Sample_Name'].astype(str)
@@ -91,13 +96,13 @@ def group_by_project(samplesheet_df: pd.DataFrame) -> dict:
     return project_dict
 
 
-def extract_run_name(sample_sheet: Path) -> str:
+def extract_run_name(samplesheet: Path) -> str:
     """
     Retrieves the 'Experiment Name' from SampleSheet.csv
-    :param sample_sheet: Path to SampleSheet.csv
+    :param samplesheet: Path to SampleSheet.csv
     :return: value of 'Experiment Name'
     """
-    with open(str(sample_sheet)) as f:
+    with open(str(samplesheet)) as f:
         for line in f:
             if 'Experiment Name' in line:
                 experiment_name = line.split(',')[1].strip()
@@ -106,7 +111,7 @@ def extract_run_name(sample_sheet: Path) -> str:
                 experiment_name = line.split(',')[1].strip()
                 return experiment_name
         else:
-            raise Exception(f"Could not find 'Experiment Name' in {sample_sheet}")
+            raise Exception(f"Could not find 'Experiment Name' in {samplesheet}")
 
 
 def check_sample_id(value: str, length: int = 15) -> bool:
@@ -154,15 +159,15 @@ def validate_sample_id(value: str, length: int = 15) -> bool:
         return True
 
 
-def generate_sample_objects(sample_sheet: Path) -> [SampleDataObject]:
-    df = read_samplesheet(sample_sheet=sample_sheet)
+def generate_sample_objects(samplesheet: Path) -> [SampleDataObject]:
+    df = read_samplesheet(samplesheet=samplesheet)
 
     # Validate header
     validate_samplesheet_header(header=list(df))
     logger.info("PASS: Header is valid")
 
     # Grab Run Name
-    run_id = extract_run_name(sample_sheet=sample_sheet)
+    run_id = extract_run_name(samplesheet=samplesheet)
     logger.info(f"Detected the following Run name: {run_id}")
 
     # Get all Projects and associated samples from the SampleSheet
@@ -197,5 +202,47 @@ def generate_sample_objects(sample_sheet: Path) -> [SampleDataObject]:
                                                  project_id=project_id,
                                                  sample_type=sample_type)
                 sample_object_list.append(sample_object)
-
     return sample_object_list
+
+
+def extract_samplesheet_header_lines(samplesheet: Path):
+    """
+    Takes an input SampleSheet.csv file and extracts the lines between the [Header] and [Reads] subheaders.
+    Fairly brittle approach with the assumption that Illumina won't change their SampleSheet format.
+    :param samplesheet: Path to SampleSheet.csv generated by MiSeq or iSeq
+    :return: List containing header lines
+    """
+    samplesheet_headers = []
+    with open(str(samplesheet), 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line == '[Header]':
+                continue
+            elif line == '[Reads]':
+                break
+            samplesheet_headers.append(line)
+    return samplesheet_headers
+
+
+def samplesheet_headers_to_dict(samplesheet_headers: list) -> dict:
+    """
+    Given a list of headers extracted with extract_samplesheet_header_fields, will parse them into a dictionary
+    :param samplesheet_headers: List of header lines
+    :return: Dictionary containing data that can be fed directly into the RunSamplesheet model (keys are attributes)
+    """
+    attr_dict = {}
+    for line in samplesheet_headers:
+        components = line.split(',')
+        _attr = components[0].lower().replace(" ", "_")
+        try:
+            _val = components[1]
+        except IndexError:
+            _val = ""
+        attr_dict[_attr] = _val
+    return attr_dict
+
+
+def extract_samplesheet_headers(samplesheet: Path) -> dict:
+    samplesheet_headers = extract_samplesheet_header_lines(samplesheet=samplesheet)
+    attr_dict = samplesheet_headers_to_dict(samplesheet_headers=samplesheet_headers)
+    return attr_dict
