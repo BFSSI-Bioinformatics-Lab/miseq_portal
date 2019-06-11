644746c93295b9dd99f7e2ce4d62eac30a7a46ca
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-06-11 09:06:29 -0400

    Added new custom filter for samplesheet header

--- miseq_portal/miseq_viewer/templatetags/miseq_viewer_extras.py
+++ miseq_portal/miseq_viewer/templatetags/miseq_viewer_extras.py
@@ -4,3 +4,8 @@ from django.template.defaulttags import register
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+@register.filter
+def parse_samplesheet_header_value(val):
+    return val.replace("_", " ").title()
