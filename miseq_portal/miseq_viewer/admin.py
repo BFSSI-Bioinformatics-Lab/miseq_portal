a9b53daceb010e85855750a6ebc708e323269c10
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-06-26 13:48:39 -0400

    Registered new SampleSheetSampleData model

--- miseq_portal/miseq_viewer/admin.py
+++ miseq_portal/miseq_viewer/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 
 from miseq_portal.miseq_viewer.models import Project, Run, RunInterOpData, Sample, UserProjectRelationship, \
-    SampleLogData, SampleAssemblyData, MergedSampleComponentGroup, MergedSampleComponent, RunSamplesheet
+    SampleLogData, SampleAssemblyData, MergedSampleComponentGroup, MergedSampleComponent, RunSamplesheet, \
+    SampleSheetSampleData
 
 
 class SampleAdmin(admin.ModelAdmin):
     search_fields = ['sample_id']
 
 
-class SampleAssemblyDataAdmin(admin.ModelAdmin):
+class SampleOneToOneAdmin(admin.ModelAdmin):
     search_fields = ['sample_id__sample_id']
 
 
@@ -18,8 +19,9 @@ admin.site.register(Run)
 admin.site.register(RunSamplesheet)
 admin.site.register(RunInterOpData)
 admin.site.register(Sample, SampleAdmin)
+admin.site.register(SampleSheetSampleData, SampleOneToOneAdmin)
 admin.site.register(UserProjectRelationship)
-admin.site.register(SampleLogData)
-admin.site.register(SampleAssemblyData, SampleAssemblyDataAdmin)
+admin.site.register(SampleLogData, SampleOneToOneAdmin)
+admin.site.register(SampleAssemblyData, SampleOneToOneAdmin)
 admin.site.register(MergedSampleComponent)
 admin.site.register(MergedSampleComponentGroup)
