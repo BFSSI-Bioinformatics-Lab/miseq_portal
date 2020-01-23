9f32753d6f01e39b335338bcb20a98579d696b63
kelseykirkland <kelseykirklandwork@gmail.com>
2020-01-23 15:11:30 -0500

    create new sample_downloader app

--- config/settings/base.py
+++ config/settings/base.py
@@ -91,6 +91,7 @@ LOCAL_APPS = [
     'miseq_portal.sample_merge.apps.SampleMergeConfig',
     'miseq_portal.miseq_uploader.apps.MiseqUploaderConfig',
     'miseq_portal.sample_workbooks.apps.SampleWorkbooksConfig',
+    'miseq_portal.sample_downloader.apps.SampleDownloaderConfig',
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
