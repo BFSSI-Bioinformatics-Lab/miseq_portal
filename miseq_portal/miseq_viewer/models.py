f016944726a940b8a51e11714e44cbf529e4d685
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-03-03 13:45:12 -0500

    Update with new MinION support

--- miseq_portal/miseq_viewer/models.py
+++ miseq_portal/miseq_viewer/models.py
@@ -15,6 +15,58 @@ from miseq_portal.users.models import User
 logger = logging.getLogger('django')
 
 
+def validate_hybrid_sample_id(value: str, length: int = 15):
+    """
+      Strict validation of Hybrid MiSeq/MinION Sample ID
+      :param value: sample_id
+      :param length: expected length of string
+      """
+    components = value.split("-")
+
+    if len(value) != length:
+        raise ValidationError(f"Hybrid Sample ID '{value}' does not meet the expected length of 15 characters. "
+                              f"Hybrid Sample ID must be in the following format: 'MIN-2020-000001'")
+    if len(components) != 3:
+        raise ValidationError(f"Hybrid Sample ID '{value}' does not appear to meet expected format. "
+                              f"Hybrid Sample ID must be in the following format: 'BMH-2018-000001'")
+    elif components[0] != 'HYB':
+        raise ValidationError(
+            f"TEXT component of Hybrid Sample ID ('{components[0]}') does not equal expected 'MIN'")
+    elif not components[1].isdigit() or len(components[1]) != 4:
+        raise ValidationError(
+            f"YEAR component of Hybrid Sample ID ('{components[1]}') does not equal expected 'YYYY' format")
+    elif not components[2].isdigit() or len(components[2]) != 6:
+        raise ValidationError(
+            f"ID component of Hybrid Sample ID ('{components[2]}') does not equal expected 'XXXXXX' format")
+    return True
+
+
+def validate_minion_sample_id(value: str, length: int = 15):
+    """
+      Strict validation of BMH MinION Sample ID
+      :param value: sample_id
+      :param length: expected length of string
+      """
+    components = value.split("-")
+
+    if len(value) != length:
+        raise ValidationError(f"MinION Sample ID '{value}' does not meet the expected length of 15 characters. "
+                              f"MinION Sample ID must be in the following format: 'MIN-2020-000001'")
+    if len(components) != 3:
+        raise ValidationError(f"MinION Sample ID '{value}' does not appear to meet expected format. "
+                              f"MinION Sample ID must be in the following format: 'BMH-2018-000001'")
+    elif components[0] != 'MIN':
+        raise ValidationError(
+            f"TEXT component of MinION Sample ID ('{components[0]}') does not equal expected 'MIN'")
+    elif not components[1].isdigit() or len(components[1]) != 4:
+        raise ValidationError(
+            f"YEAR component of MinION Sample ID ('{components[1]}') does not equal expected 'YYYY' format")
+    elif not components[2].isdigit() or len(components[2]) != 6:
+        raise ValidationError(
+            f"ID component of MinION Sample ID ('{components[2]}') does not equal expected 'XXXXXX' format")
+    return True
+
+
 def validate_sample_id(value: str, length: int = 15):
     """
     Strict validation of BMH Sample ID
@@ -141,6 +193,16 @@ class RunDataObject:
     qmetrics: Path = None
     tilemetrics: Path = None
     logfiles: list = None
+
+
+@dataclass
+class MinIONRunDataObject:
+    """
+    Dataclass to store metadata for a run
+    """
+    run_id: str
+    run_type: str = None
+    sample_sheet: Path = None
 
 
 # Create your models here.
@@ -294,6 +356,50 @@ class RunInterOpData(TimeStampedModel):
         verbose_name_plural = 'Run InterOp Data'
 
 
+class MinIONRun(TimeStampedModel):
+    """
+    Stores information relating to a single sequencing run
+    """
+    run_id = models.CharField(max_length=256, unique=True)
+
+    @property
+    def run_url(self) -> str:
+        return reverse('miseq_viewer:miseq_viewer_run_detail', args=(self.pk,))
+
+    @property
+    def num_samples(self) -> int:
+        return len(MinIONSample.objects.filter(run_id=self.pk))
+
+    def __str__(self):
+        return str(self.run_id)
+
+    def __len__(self):
+        return self.num_samples
+
+    class Meta:
+        verbose_name = 'MinION Run'
+        verbose_name_plural = 'MinION Runs'
+
+
+class MinIONRunSamplesheet(TimeStampedModel):
+    run_id = models.OneToOneField(MinIONRun, on_delete=models.CASCADE, primary_key=True)
+    sample_sheet = models.FileField(upload_to=upload_run_file, blank=True, max_length=1000)
+
+    def get_fields(self):
+        return [(field.name, field.value_to_string(self)) for field in MinIONRunSamplesheet._meta.fields]
+
+    def read_samplesheet(self):
+        """ Method to read in an input .xlsx """
+        pass
+
+    class Meta:
+        verbose_name = 'MinION Run Samplesheet'
+        verbose_name_plural = 'MinION Run Samplesheets'
+
+    def __str__(self):
+        return str(self.run_id)
+
+
 class MergedSampleComponentGroup(models.Model):
     """
     Model for reference by MergedSample.
@@ -315,6 +421,23 @@ class MergedSampleComponentGroup(models.Model):
     class Meta:
         verbose_name = 'Merged Sample Component Group'
         verbose_name_plural = 'Merged Sample Component Groups'
+
+
+class MinIONSample(TimeStampedModel):
+    # Basic sample info
+    sample_id = models.CharField(max_length=15, unique=True, validators=[validate_minion_sample_id])
+    sample_name = models.TextField(blank=True)
+    long_reads = models.FileField(upload_to=upload_reads, blank=True, max_length=1000)
+
+    # Project/MinIONRun
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE, blank=True, null=True)
+    run_id = models.ForeignKey(MinIONRun, on_delete=models.CASCADE, blank=True, null=True)
+
+    # Metadata from the samplesheet
+    run_protocol = models.CharField(blank=True, max_length=1028)
+    instrument_id = models.CharField(blank=True, max_length=1028)
+    sequencing_kit = models.CharField(blank=True, max_length=1028)
+    flowcell_type = models.CharField(blank=True, max_length=1028)
 
 
 class Sample(TimeStampedModel):
@@ -578,3 +701,9 @@ class SampleAssemblyData(TimeStampedModel):
     class Meta:
         verbose_name = 'Sample Assembly Data'
         verbose_name_plural = 'Sample Assembly Data'
+
+
+class MiSeqMinIONPair(models.Model):
+    sample_id = models.CharField(max_length=15, unique=True, validators=[validate_hybrid_sample_id])
+    miseq_sample = models.ForeignKey(Sample, on_delete=models.CASCADE)
+    minion_sample = models.ForeignKey(MinIONSample, on_delete=models.CASCADE)
