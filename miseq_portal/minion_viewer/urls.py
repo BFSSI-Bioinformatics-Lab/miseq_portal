d62ada835fc0d78c43d274a916590a49bba4b6b2
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-03-09 15:00:04 -0400

    Add run detail, sample detail, api URLs

--- miseq_portal/minion_viewer/urls.py
+++ miseq_portal/minion_viewer/urls.py
@@ -3,14 +3,27 @@ from rest_framework import routers
 
 from miseq_portal.minion_viewer.views import (
     minion_viewer_index_view,
+    minion_run_list_view,
+    minion_sample_detail_view,
+    minion_run_detail_view,
+    MinIONRunViewSet,
+    MinIONRunSamplesheetViewSet,
+    MinIONSampleViewSet,
 )
 
 app_name = "minion_viewer"
 
 # Django-rest-framework (https://django-rest-framework-datatables.readthedocs.io/en/latest/tutorial.html)
 router = routers.DefaultRouter()
-# router.register(r'workbooks', WorkbookViewSet, base_name='workbooks-detail')
+router.register(r'minion_runs', MinIONRunViewSet, base_name='minion-run')
+router.register(r'minion_samples', MinIONSampleViewSet, base_name='minion-sample')
+router.register(r'minion_samplesheets', MinIONRunSamplesheetViewSet, base_name='minion-samplesheet')
 
 urlpatterns = [
     path("", view=minion_viewer_index_view, name="minion_viewer_index"),
+    re_path("^api/", include(router.urls), name="minion_viewer_api"),
+    path("runs/", view=minion_run_list_view, name="minion_viewer_runs"),
+    re_path("^run/(?P<pk>\d+)$", view=minion_run_detail_view, name="minion_run_detail"),
+    re_path("^sample/(?P<pk>\d+)$", view=minion_sample_detail_view,
+            name="minion_sample_detail")
 ]
