ab5e0f193aa8661adb39fda832b55dcd9589b3a1
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-03-09 15:01:29 -0400

    Add URLs for chunked upload

--- miseq_portal/minion_uploader/urls.py
+++ miseq_portal/minion_uploader/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path, re_path, include
+from django.urls import path
 from rest_framework import routers
 
 from miseq_portal.minion_uploader.views import (
-    minion_uploader_index_view,
+    MinIONUploaderIndexView,
+    UploadSuccessView,
+    MinIONRunChunkedUploadView,
+    MinIONRunChunkedUploadCompleteView
 )
 
 app_name = "minion_uploader"
@@ -12,5 +15,9 @@ router = routers.DefaultRouter()
 # router.register(r'workbooks', WorkbookViewSet, base_name='workbooks-detail')
 
 urlpatterns = [
-    path("", view=minion_uploader_index_view, name="minion_uploader_index"),
+    path("", view=MinIONUploaderIndexView.as_view(), name="minion_uploader_index"),
+    path("upload_success/", view=UploadSuccessView.as_view(), name="upload_success"),
+    path("api/chunked_upload_complete/", view=MinIONRunChunkedUploadCompleteView.as_view(),
+         name="api_chunked_upload_complete"),
+    path("api/chunked_upload/", view=MinIONRunChunkedUploadView.as_view(), name="api_chunked_upload"),
 ]
