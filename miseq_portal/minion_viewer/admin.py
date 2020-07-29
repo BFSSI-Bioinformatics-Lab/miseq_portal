795ba2a1ba2d9113399406ee77af2ecffd74bab3
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-07-29 13:16:56 -0400

    Register models in admin panel

--- miseq_portal/minion_viewer/admin.py
+++ miseq_portal/minion_viewer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from miseq_portal.minion_viewer.models import MinIONRun, MinIONRunSamplesheet, MinIONSample
 
 # Register your models here.
+admin.site.register(MinIONRun)
+admin.site.register(MinIONRunSamplesheet)
+admin.site.register(MinIONSample)
