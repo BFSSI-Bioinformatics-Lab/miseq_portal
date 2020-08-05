4edd50b9930d7d524d6a0a9783f4289424b688a8
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-08-05 17:55:23 -0400

    Refactor templatetag extras

--- miseq_portal/analysis/templatetags/analysis_extras.py
+++ miseq_portal/analysis/templatetags/analysis_extras.py
@@ -1,10 +1,13 @@
-from django.template.defaulttags import register
-
+from django import template
+from django.template.defaultfilters import stringfilter
 from config.settings.base import MEDIA_ROOT
+
+register = template.Library()
 
 """ Note that these tags were registered in config.settings.base in the TEMPLATES section under libraries """
 
 
 @register.filter
+@stringfilter
 def filter_media_root_from_url(val):
     return val.replace(str(MEDIA_ROOT), "")
